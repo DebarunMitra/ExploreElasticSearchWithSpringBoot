8cd0b2055b054f4698cf9f25a246475e4f111c9f
Debarun Mitra <debarun96mitra@gmail.com>
2021-07-06 22:30:52 +0530

    project dependency added

---
+++ studentDataElasticsearchEx/src/test/java/com/esExampleTwo/studentDataElasticsearchEx/StudentDataElasticsearchExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esExampleTwo.studentDataElasticsearchEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDataElasticsearchExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
