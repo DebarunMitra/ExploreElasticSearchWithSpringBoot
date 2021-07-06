83ce993a81a99fd1f54930f04ee67045f0e07dad
Debarun Mitra <debarun96mitra@gmail.com>
2021-07-06 22:27:17 +0530

    helpers added

---
+++ studentDataElasticsearchEx/src/main/java/com/esExampleTwo/studentDataElasticsearchEx/helper/Indices.java
@@ -0,0 +1,10 @@
+package com.esExampleTwo.studentDataElasticsearchEx.helper;
+
+public class Indices {
+
+    public static final String STUDENT_INDEX = "student";
+
+    public static final String BOOK_INDEX = "book";
+
+    public static final String INTERNSHIP_INDEX = "hello_interns";
+}
